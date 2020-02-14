324e876da8bfb95cc2128cad6ea78a88ba92a7a9
Mason <mason.a.traylor@gmail.com>
2020-02-14 07:42:20 -1000

    Add WebVR ability for phones

--- app/javascript/bundles/Draw/components/MyCanvas.jsx
+++ app/javascript/bundles/Draw/components/MyCanvas.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Canvas } from 'react-three-fiber'
 import MyControls from './MyControls'
 import Cloud from './Cloud'
+import { VRButton } from 'three/examples/jsm/webxr/VRButton'
+
 
 export default function MyCanvas(props) {
   return(
     <Canvas className='h-100' 
       camera={{position:[0, 0, 2]}} 
-      gl={{ alpha: false, antialias: false, logarithmicDepthBuffer: true }} >
+      gl={{ alpha: false, antialias: false, logarithmicDepthBuffer: true }}
+      vr
+      onCreated={({ gl }) => document.body.appendChild(VRButton.createButton(gl))} >
 
       <Cloud {...props}/>
       <MyControls />
