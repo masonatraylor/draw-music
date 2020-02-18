61de93695de341d05cfabb582b13e72697932e5e
Mason <mason.a.traylor@gmail.com>
2020-02-17 14:09:42 -1000

    remove Box.jsx

--- app/javascript/packs/draw-bundle.js
+++ app/javascript/packs/draw-bundle.js
@@ -1,13 +1,11 @@
 import ReactOnRails from 'react-on-rails';
 
-import Box from '../bundles/Draw/components/Box';
 import Cloud from '../bundles/Draw/components/Cloud';
 import MyCanvas from '../bundles/Draw/components/MyCanvas';
 import FullScreenVisualizer from '../bundles/Draw/components/FullScreenVisualizer';
 
 // This is how react_on_rails can see the DrawCanvas in the browser.
 ReactOnRails.register({
-  Box,
   Cloud,
   MyCanvas,
   FullScreenVisualizer
